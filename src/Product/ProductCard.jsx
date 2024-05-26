a88b36d2d2011b1be608ed713056c3b73569c63f
pritam guha <pritam@tenxerlabs.com>
2024-05-26 21:28:26 +0530

    fixed the alingh

--- src/Product/ProductCard.jsx
+++ src/Product/ProductCard.jsx
@@ -12,12 +12,14 @@ function ProductCard() {
       {products.map((element) => (
         <figure key={element.id} className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
           <a href="#" onClick={() => handelProductSelect(element)}>
-            <img className="w-full object-cover" src={element.image} alt="" />
+            <img className="w-full h-[22rem] object-cover" src={element.image} alt="" />
             <figcaption className="pt-4">
-              <h3 className="text-xl mb-1">{element.title}</h3>
-              <p className="text-[#575A6E] text-sm mb-2">{element.category}</p>
-              <div className="flex items-center space-x-1 mb-5">
-                <Rating value={element.rating.rate} />
+              <div className='h-[7rem]'>
+                <h3 className="text-xl mb-1">{element.title}</h3>
+                <p className="text-[#575A6E] text-sm mb-2">{element.category}</p>
+                <div className="flex items-center space-x-1 mb-5">
+                  <Rating value={element.rating.rate} />
+                </div>
               </div>
               <a className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
                 href="#" onClick={(e) => handelAddToCart(e, element)}>
